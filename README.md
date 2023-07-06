7682a4d97419d579484a4c2358018a921d46c486
姚恒锋 <1921934563@qq.com>
2023-07-06 18:36:27 +0800

    feat: 更新windows数据测试

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: yaohengfeng 1921934563@qq.com
  * @Date: 2023-01-13 11:09:26
  * @LastEditors: 姚恒锋 1921934563@qq.com
- * @LastEditTime: 2023-06-07 11:20:40
+ * @LastEditTime: 2023-07-06 09:40:34
  * @FilePath: \hid-handle\README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
--- writeFile.js
+++ writeFile.js
@@ -2,7 +2,7 @@
  * @Author: yaohengfeng 1921934563@qq.com
  * @Date: 2023-01-13 10:53:22
  * @LastEditors: 姚恒锋 1921934563@qq.com
- * @LastEditTime: 2023-06-12 16:27:38
+ * @LastEditTime: 2023-07-05 17:38:27
  * @FilePath: \hid-handle\test.js
  * @Description: test.js
  */
@@ -14,12 +14,12 @@ console.info("handle: ", hidHandle);
 
 console.info("============================================================");
 
-// // 写入文件更新UI
-// console.info(
-//   hidHandle.hid_write_file_handle("./new_ui/hbin/hmi_res.hbin", "hmi_res.hbin", 1)
-// )
+// 写入文件更新UI
+console.info(
+  hidHandle.hid_write_file_handle("./new_ui/hbin/hmi_res.hbin", "hmi_res.hbin", 1)
+)
 
 // 更新固件
-console.info(
-  hidHandle.hid_write_file_handle("./product_sabbat_dual.abs", "product_sabbat_dual.abs", 1)
-)
+// console.info(
+//   hidHandle.hid_write_file_handle("./product_sabbat_dual.abs", "product_sabbat_dual.abs", 1)
+// )
